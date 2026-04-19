1c5a91f217e9eaeb5cfceba875afba2e32eff1af
Daniel Portnoy <danielportnoy@users.noreply.github.com>
2026-04-19 16:10:27 +0300

    Add: python-stack-push-pop - submission-0

---
+++ Python Coding Interviews/python-stack-push-pop/submission-0.py
@@ -0,0 +1,14 @@
+from typing import List
+
+
+def reverse_list(arr: List[int]) -> List[int]:
+    reversed = []
+    for i in range(len(arr) - 1, -1, -1):
+        reversed.append(arr[i])
+    return reversed
+
+
+# do not modify below this line
+print(reverse_list([1, 2, 3]))
+print(reverse_list([3, 2, 1, 4, 6, 2]))
+print(reverse_list([1, 9, 7, 3, 2, 1, 4, 6, 2]))
