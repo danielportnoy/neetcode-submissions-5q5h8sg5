812eb34f1be6e246ee1969f39535f7a761cefd3f
Daniel Portnoy <danielportnoy@users.noreply.github.com>
2026-05-08 23:43:56 +0300

    Add: decode-ways - submission-1

---
+++ Data Structures & Algorithms/decode-ways/submission-1.py
@@ -0,0 +1,50 @@
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        validSet = (
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10",
+            "11",
+            "12",
+            "13",
+            "14",
+            "15",
+            "16",
+            "17",
+            "18",
+            "19",
+            "20",
+            "21",
+            "22",
+            "23",
+            "24",
+            "25",
+            "26",
+        )
+
+        if len(s) == 1:
+            if s == "0":
+                return 0
+            return 1
+
+        memo = [1, 1]
+
+        for i in range(len(s)):
+            ways = 0
+            # case 1 - take last digit
+            if s[i] in validSet:
+                ways += memo[i + 1]
+            # case 2 - take 2 last digits
+            if i >= 1 and s[i - 1 : i + 1] in validSet:
+                ways += memo[i]
+
+            memo.append(ways)
+
+        return memo[-1]
